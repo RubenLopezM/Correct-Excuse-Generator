f44e3f42385aa7a06561eb3436268bada7c7e5cc
RubenLopezM <rublopmag@hotmail.com>
2021-10-13 14:39:07 +0000

    Changed file js

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-var who=['The dog','A ghost','My friend','The cat']
-var action=['broke','stole','took']
-var what=['my phone','the keys','my credit card','my homework', 'my laptop']
-var when=['this morning','five minutes before','before the class','during the weekend']
+const WHO=['The dog','A ghost','My friend','The cat']
+const ACTION=['broke','stole','took']
+const WHAT=['my phone','the keys','my credit card','my homework', 'my laptop']
+const WHEN=['this morning','five minutes before','before the class','during the weekend']
 
 
 let excuseEl=document.getElementById("excuse")
 
  function number(array){
     
-    var random_number= Math.floor(Math.random()*array.length)
+    let random_number= Math.floor(Math.random()*array.length)
     return random_number
  }
 
 function generarexcusa(){
-    var excuse=who[number(who)]+' '+action[number(action)]+' '+what[number(what)]+' '+when[number(when)]
+    let excuse=who[number(who)]+' '+action[number(action)]+' '+what[number(what)]+' '+when[number(when)]
     excuseEl.innerHTML= excuse
 }
